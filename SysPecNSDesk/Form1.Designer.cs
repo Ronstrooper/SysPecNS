105113d9e5f6a2d7e38de93a2a544aad4b5bc31a
rodrigo.srodrigues24 <rodrigo.srodrigues24@ITQ0675991W10-1>
2024-08-19 09:10:51 -0300

    Adicionar arquivos de projeto.

---
+++ SysPecNSDesk/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SysPecNSDesk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button button1;
+            textBox1 = new TextBox();
+            lsResultado = new ListBox();
+            cmbOperacao = new ComboBox();
+            txtNumero2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new Point(428, 144);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Confirmar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(216, 144);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // lsResultado
+            // 
+            lsResultado.FormattingEnabled = true;
+            lsResultado.ItemHeight = 15;
+            lsResultado.Location = new Point(86, 183);
+            lsResultado.Name = "lsResultado";
+            lsResultado.Size = new Size(178, 214);
+            lsResultado.TabIndex = 2;
+            lsResultado.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // cmbOperacao
+            // 
+            cmbOperacao.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOperacao.FormattingEnabled = true;
+            cmbOperacao.Items.AddRange(new object[] { "Multiplicação", "Divisão", "Adição", "Subtração", "Exponenciação", "Radiciação", "Logaritmos" });
+            cmbOperacao.Location = new Point(86, 144);
+            cmbOperacao.Name = "cmbOperacao";
+            cmbOperacao.Size = new Size(121, 23);
+            cmbOperacao.TabIndex = 3;
+            cmbOperacao.SelectedIndexChanged += cmbOperacao_SelectedIndexChanged;
+            // 
+            // txtNumero2
+            // 
+            txtNumero2.Location = new Point(322, 144);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 4;
+            txtNumero2.TextChanged += txtNumero2_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(86, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Operação";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(216, 120);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Valor";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(324, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Valor 2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNumero2);
+            Controls.Add(cmbOperacao);
+            Controls.Add(lsResultado);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private ListBox lsResultado;
+        private ComboBox cmbOperacao;
+        private TextBox txtNumero2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ SysPecNSDesk/Form1.cs
@@ -0,0 +1,101 @@
+namespace SysPecNSDesk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Olá TI96");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 && cmbOperacao.SelectedIndex > 4)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string operacao = cmbOperacao.Text;
+            int posicaoOperacao = cmbOperacao.SelectedIndex;
+            double numero1 = 0;
+            if (textBox1.Text != string.Empty)
+            {
+                numero1 = Convert.ToDouble(textBox1.Text);
+            }
+
+            double numero2 = 0;
+            if (posicaoOperacao < 5)
+            {
+                if (txtNumero2.Text != string.Empty)
+                {
+                    numero2 = Convert.ToDouble(txtNumero2.Text);
+                }
+            }
+            double resultado = 0;
+            switch (posicaoOperacao)
+            {
+                case 0:
+                    resultado = numero1 * numero2;
+                    break;
+                case 1:
+                    resultado = numero1 / numero2;
+                    break;
+                case 2:
+                    resultado = numero1 + numero2;
+                    break;
+                case 3:
+                    resultado = numero1 - numero2;
+                    break;
+                case 4:
+                    resultado = Math.Pow(numero1, numero2);
+                    break;
+                case 5:
+                    resultado = Math.Sqrt(numero1);
+                    break;
+                case 6:
+                    resultado = Math.Log(numero1);
+                    break;
+                default:
+                    break;
+            }
+            lsResultado.Items.Add($"{operacao} - {posicaoOperacao} = {resultado}");
+            textBox1.Clear();
+            txtNumero2.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbOperacao_SelectedIndexChanged(object sender, EventArgs e) //botão confirmar
+        {
+            if (cmbOperacao.SelectedIndex > 4)
+            {
+                txtNumero2.Enabled = true;
+            }
+            else
+            {
+                txtNumero2.Enabled = false;
+            }
+            textBox1.Clear(); //limpar caixa
+            txtNumero2.Clear();
+            textBox1.Focus();// barra piscando
+            txtNumero2.Enabled= true;
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
