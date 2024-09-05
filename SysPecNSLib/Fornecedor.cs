343ccb632d8d6cfeb3a76abf1cde2b49dd10dbd3
RODRIGO SANTOS RODRIGUES <rodrigo_wakashimazu@hotmail.com>
2024-09-05 10:31:48 -0300

    alterações Fornecedor e ProdutoFornecedor

--- SysPecNSLib/Fornecedor.cs
+++ SysPecNSLib/Fornecedor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,47 @@ namespace SysPecNSLib
 {
     public class Fornecedor
     {
+        public int Id { get; set; }
+        public string? RazaoSocial { get; set; }
+        public string? Fantasia { get; set; }
+        public string? CNPJ { get; set; }
+        public string? Contato { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+        public Fornecedor() { }
+        public Fornecedor(int id, string? razaoSocial, string? fantasia, string? cnpj, string? contato, string? telefone, string? email)
+        {
+            Id = id;
+            RazaoSocial = razaoSocial;
+            Fantasia = fantasia;
+            CNPJ = cnpj;
+            Contato = contato;
+            Telefone = telefone;
+            Email = email;
+        }
+        public static Fornecedor ObterPorId(int id)
+        {
+            Fornecedor fornecedor = null;
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select id, razao_social, fantasia, cnpj, contato, telefone, email from fornecedores where id = {id}";
+
+            var dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                fornecedor = new Fornecedor(
+                    dr.GetInt32(0),
+                    dr.GetInt32(1),
+                    dr.GetInt32(2),
+                    dr.GetInt32(3),
+                    dr.GetInt32(4),
+                    dr.GetInt32(5),
+                    dr.GetInt32(6)
+                    );
+            }
+            cmd.Connection.Close();
+            return fornecedor;
+        }
+
     }
 }
---
+++ SysPecNSLib/ProdutoFornecedor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPecNSLib
+{
+    public class ProdutoFornecedor
+    {
+        public int ProdutoId { get; set; }
+        public int FornecedorId { get; set; }
+
+        public ProdutoFornecedor(){}
+        public ProdutoFornecedor (int produtoid, int fornecedorid)
+        {
+            ProdutoId = produtoId;
+            FornecedorId = fornecedorId;
+        }
+        public static List<ProdutoFornecedor>ObterListaPorFornecedor(int fornecedorId)
+        {
+            List<ProdutoFornecedor> lista = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select produto_id, fornecedor_id from produtofornecedor where fornecedor_id = {fornecedorId}";
+
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(newProdutoFornecedor)
+            }
+        }
+    }
+}
