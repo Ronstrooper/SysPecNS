5326db6892d6e3e3bdf0f83defc5e7e902a1744a
RODRIGO SANTOS RODRIGUES <rodrigo_wakashimazu@hotmail.com>
2024-09-05 11:30:40 -0300

    Necessário corrigir a classe Fornecedor

--- SysPecNSLib/Fornecedor.cs
+++ SysPecNSLib/Fornecedor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,25 +16,26 @@ namespace SysPecNSLib
         public string? Telefone { get; set; }
         public string? Email { get; set; }
         public Fornecedor() { }
-        public Fornecedor(int id, string? razaoSocial, string? fantasia, string? cnpj, string? contato, string? telefone, string? email)
+        public Fornecedor(int id, string? razaoSocial, string? fantasia, string? cNPJ, string? contato, string? telefone, string? email)
         {
             Id = id;
             RazaoSocial = razaoSocial;
             Fantasia = fantasia;
-            CNPJ = cnpj;
+            CNPJ = cNPJ;
             Contato = contato;
             Telefone = telefone;
             Email = email;
         }
+
         public static Fornecedor ObterPorId(int id)
         {
             Fornecedor fornecedor = null;
             var cmd = Banco.Abrir();
-            cmd.CommandType = CommandType.Text;
+            cmd.CommandType = System.Data.CommandType.Text;
             cmd.CommandText = $"select id, razao_social, fantasia, cnpj, contato, telefone, email from fornecedores where id = {id}";
 
             var dr = cmd.ExecuteReader();
-            if(dr.Read())
+            if (dr.Read())
             {
                 fornecedor = new Fornecedor(
                     dr.GetInt32(0),
@@ -50,6 +50,5 @@ namespace SysPecNSLib
             cmd.Connection.Close();
             return fornecedor;
         }
-
     }
 }
