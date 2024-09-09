e149d4f6791917814c685914312804901a260c70
Rodrigo <rodrigo_wakashimazu@hotmail.com>
2024-09-09 20:57:26 -0300

    FrmCliente - Aba Cliente pronta

---
+++ SysPecNSDesk/FrmClientes.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace SysPecNSDesk
+{
+    partial class FrmClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            txtId = new TextBox();
+            txtTelefone = new TextBox();
+            txtEmail = new TextBox();
+            txtCPF = new TextBox();
+            txtNome = new TextBox();
+            dtnDataNasc = new DateTimePicker();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            checkBox1 = new CheckBox();
+            btnInserir = new Button();
+            textBox6 = new TextBox();
+            dataGridView1 = new DataGridView();
+            Id = new DataGridViewTextBoxColumn();
+            Nome = new DataGridViewTextBoxColumn();
+            Cpf = new DataGridViewTextBoxColumn();
+            TELEFONE = new DataGridViewTextBoxColumn();
+            Email = new DataGridViewTextBoxColumn();
+            Data_nasc = new DataGridViewTextBoxColumn();
+            Data_cad = new DataGridViewTextBoxColumn();
+            Ativo = new DataGridViewCheckBoxColumn();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(2, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(908, 541);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Controls.Add(textBox6);
+            tabPage1.Controls.Add(btnInserir);
+            tabPage1.Controls.Add(checkBox1);
+            tabPage1.Controls.Add(label7);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(dtnDataNasc);
+            tabPage1.Controls.Add(txtNome);
+            tabPage1.Controls.Add(txtCPF);
+            tabPage1.Controls.Add(txtEmail);
+            tabPage1.Controls.Add(txtTelefone);
+            tabPage1.Controls.Add(txtId);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(900, 513);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(900, 513);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(26, 66);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(100, 23);
+            txtId.TabIndex = 0;
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.Location = new Point(307, 142);
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(192, 23);
+            txtTelefone.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(26, 142);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(251, 23);
+            txtEmail.TabIndex = 2;
+            // 
+            // txtCPF
+            // 
+            txtCPF.Location = new Point(523, 66);
+            txtCPF.Name = "txtCPF";
+            txtCPF.Size = new Size(162, 23);
+            txtCPF.TabIndex = 3;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(162, 66);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(337, 23);
+            txtNome.TabIndex = 4;
+            // 
+            // dtnDataNasc
+            // 
+            dtnDataNasc.Location = new Point(523, 142);
+            dtnDataNasc.Name = "dtnDataNasc";
+            dtnDataNasc.Size = new Size(264, 23);
+            dtnDataNasc.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Id";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(162, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(523, 39);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 15);
+            label3.TabIndex = 8;
+            label3.Text = "CPF";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 111);
+            label4.Name = "label4";
+            label4.Size = new Size(36, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Email";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(307, 111);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Telefone";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(523, 111);
+            label6.Name = "label6";
+            label6.Size = new Size(114, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Data de Nascimento";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(27, 194);
+            label7.Name = "label7";
+            label7.Size = new Size(35, 15);
+            label7.TabIndex = 12;
+            label7.Text = "Ativo";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(29, 218);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 13;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnInserir
+            // 
+            btnInserir.Location = new Point(127, 218);
+            btnInserir.Name = "btnInserir";
+            btnInserir.Size = new Size(75, 23);
+            btnInserir.TabIndex = 14;
+            btnInserir.Text = "Inserir";
+            btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(77, 283);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(675, 23);
+            textBox6.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Id, Nome, Cpf, TELEFONE, Email, Data_nasc, Data_cad, Ativo });
+            dataGridView1.Location = new Point(29, 321);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Size = new Size(799, 182);
+            dataGridView1.TabIndex = 16;
+            // 
+            // Id
+            // 
+            Id.HeaderText = "ID";
+            Id.Name = "Id";
+            Id.ReadOnly = true;
+            // 
+            // Nome
+            // 
+            Nome.HeaderText = "NOME";
+            Nome.Name = "Nome";
+            Nome.ReadOnly = true;
+            // 
+            // Cpf
+            // 
+            Cpf.HeaderText = "CPF";
+            Cpf.Name = "Cpf";
+            Cpf.ReadOnly = true;
+            // 
+            // TELEFONE
+            // 
+            TELEFONE.HeaderText = "Telefone";
+            TELEFONE.Name = "TELEFONE";
+            TELEFONE.ReadOnly = true;
+            // 
+            // Email
+            // 
+            Email.HeaderText = "Email";
+            Email.Name = "Email";
+            Email.ReadOnly = true;
+            // 
+            // Data_nasc
+            // 
+            Data_nasc.HeaderText = "Data de Nascimento";
+            Data_nasc.Name = "Data_nasc";
+            Data_nasc.ReadOnly = true;
+            // 
+            // Data_cad
+            // 
+            Data_cad.HeaderText = "Cadastro";
+            Data_cad.Name = "Data_cad";
+            Data_cad.ReadOnly = true;
+            // 
+            // Ativo
+            // 
+            Ativo.HeaderText = "Ativo";
+            Ativo.Name = "Ativo";
+            Ativo.ReadOnly = true;
+            // 
+            // FrmClientes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(908, 541);
+            Controls.Add(tabControl1);
+            Name = "FrmClientes";
+            Text = "FrmClientes";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label2;
+        private Label label1;
+        private DateTimePicker dtnDataNasc;
+        private TextBox txtNome;
+        private TextBox txtCPF;
+        private TextBox txtEmail;
+        private TextBox txtTelefone;
+        private TextBox txtId;
+        private TabPage tabPage2;
+        private Button btnInserir;
+        private CheckBox checkBox1;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox6;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn Nome;
+        private DataGridViewTextBoxColumn Cpf;
+        private DataGridViewTextBoxColumn TELEFONE;
+        private DataGridViewTextBoxColumn Email;
+        private DataGridViewTextBoxColumn Data_nasc;
+        private DataGridViewTextBoxColumn Data_cad;
+        private DataGridViewCheckBoxColumn Ativo;
+    }
+}
